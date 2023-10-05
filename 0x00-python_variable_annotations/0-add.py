f2f115072dcf9743cd1e4e9a2fbe0ccff49547bf
AMO15310 <wachiraamos402@gmail.com>
2023-10-05 11:52:29 +0300

    task:add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Function to add two values and return the result"""
+
+def add(a:float,b:float) -> float:
+    """Takes two arguments as floats and returns a float """
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+This directory contains type annotations for python
